35f57860c178271a32b6eef001a415ff9034fa47
Oren Shklarsky <oren@neur.io>
2015-11-20 16:46:57 -0800

    Use grid search to optimize parameters.

--- svms/Prometheus.py
+++ svms/Prometheus.py
@@ -3,10 +3,13 @@ import numpy as np
 import os
 import sys
 import argparse
+import pickle
 from sklearn import svm
+from sklearn.grid_search import GridSearchCV
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import classification_report
 from scipy.spatial.distance import cdist
 from utils.notification import email, push_note
-from utils.config import config_from_file
 
 def merge(arrays, idx_to_omit):
     """
@@ -25,7 +28,7 @@ def merge(arrays, idx_to_omit):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='Prometheus svm')
     parser.add_argument('--email', nargs='*', default='')
-    parser.add_argument('--n_folds', type=int, default=25)
+    parser.add_argument('--test_size', type=float, default=0.5)
     parser.add_argument('--data_file', default='training.csv')
     parser.add_argument('--push', action='store_true')
 
@@ -41,30 +44,22 @@ if __name__ == '__main__':
 
     # number of examples (250000)
     N = len(data_rows) - 1  # first row of training.csv is column names
-    # number of features (30)
-    D = len(data_rows[0].strip().split(',')) - 3  # ignore the id, weight, and label values.
+    # number of features (30 + 1 weight)
+    D = len(data_rows[0].strip().split(',')) - 2  # ignore the id and the label. Keep the weight for now.
 
     # data is an N x D array
-    all_train_data = np.zeros((N, D))
-
-    # weight vector
-    all_train_weights = np.zeros((N, 1))
+    all_data = np.zeros((N, D))
 
     # label vector.
-    all_train_labels = np.zeros((N, 1), dtype=str)
+    all_labels = np.zeros((N, 1), dtype=str)
 
     # process the rows, and fill up the data and labels. Ignore the first row.
     for i, row in enumerate(data_rows[1:]):
         values = row.strip().split(',')
 
-        # The features are indexed one (inclusive) to the second to last (exclusive).
-        all_train_data[i] = values[1:-2]
+        all_data[i] = values[1:-1]
 
-        # the weight is in the second to last column
-        all_train_weights[i] = values[-2]
-
-        # the label is in the last column
-        all_train_labels[i] = values[-1]
+        all_labels[i] = values[-1]
 
         # show some progress
         if i > 0 and i % 10000 == 0:
@@ -73,67 +68,40 @@ if __name__ == '__main__':
 
     print('done')
 
-    # Train svm. SVC's default kernel is rbf.
-    kernel = 'sigmoid'
-    n_folds = args.n_folds
+    # we're going to use grid search to find best parameters.
+    train_data, test_data, train_labels, test_labels = train_test_split(all_data, all_labels, test_size=args.test_size)
 
-    # SVC has built-in support for sigmoid, rbf, linear, and polynomial kernels. We can make our own by using precomputed.
-    # The caveat is that we have to figure out how to hold a 250000 x 250000 kernel matrix in memory. The code below
-    # doesn't work on an 8gb machine.
-    if kernel == 'precomputed':
-        print('kernalizing...', end='')
-        s = 0.1
-        all_data_gram = np.exp(-cdist(all_train_data, all_train_data) / (2 * s ** 2))
-        print('done')
-        split_train_data = np.array_split(all_data_gram, n_folds)
-    else:
-        split_train_data = np.array_split(all_train_data, n_folds)
+    parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4], 'C': [1, 10, 100, 1e3]},
+                  {'kernel': ['sigmoid'], 'gamma': [1e-3, 1e-4], 'C': [1, 10, 100, 1e3], 'coef0': [0, 1]}]
 
-    # split the data in n_folds chunks.
-    split_train_weights = np.array_split(all_train_weights, n_folds)
-    split_train_labels = np.array_split(all_train_labels, n_folds)
 
-    # each chunk will be used as a test vector
-    for i, test_data in enumerate(split_train_data):
-        test_weights = split_train_weights[i]
-        test_labels = split_train_labels[i]
+    # separate out the weights.
+    train_weights = train_data[:, -1]
+    train_data = train_data[:, 0:-1]
 
-        # The other chunks will be the training data.
-        train_data = merge(split_train_data, idx_to_omit=i)
-        train_weights = merge(split_train_weights, idx_to_omit=i)
-        train_labels = merge(split_train_labels, idx_to_omit=i)
+    # no need to save the test weights. Right?
+    test_data = test_data[:, 0:-1]
 
-        classifier = svm.SVC(kernel=kernel,
-                             cache_size=2000,  # size of kernel cache. Bigger is faster but takes more RAM.
-                             C=1,  # When weights are used, this will be rescaled to C * weight[i] for sample i.
-                             )
-        print(classifier)
-        st_time = time.time()
-        print('fold number {} out of {}'.format(i, n_folds))
-        print('fitting...', end='')
-        sys.stdout.flush()
-        classifier.fit(train_data, train_labels.flat, sample_weight=train_weights.flat)
-        print('elapsed training time: {}'.format(time.time() - st_time))
+    classifier = GridSearchCV(svm.SVC(), parameters, cv=3, fit_params={'sample_weight': train_weights.flat})
+    classifier.fit(train_data, train_labels.flat)
 
-        # compare to test_data
-        predicted_labels = classifier.predict(test_data)
-        num_correct = np.sum(predicted_labels == test_labels.flat)
-        print('num correct: {}'.format(num_correct))
-        sys.stdout.flush()
+    # best parameters
+    print('best parameters: {}'.format(classifier.best_params_))
 
-        # notify by email
-        if args.email:
-            # get config
-            subject = 'fold number {} out of {}. kernel: {}, accuracy: {}'.format(i,
-                                                                                  n_folds,
-                                                                                  kernel,
-                                                                                  num_correct / len(test_labels))
-            email(to_addrs=args.email, subject=subject, body='')
+    for params, mean_score, scores in classifier.grid_scores_:
+        print('{:.3f} (+/-{:.03f}) for {}'.format(mean_score, scores.std() * 2, params))
 
-        if args.push:
-            subject = 'training fold done'
-            body = 'fold number {} out of {}. kernel: {}, accuracy: {}'.format(i,
-                                                                                  n_folds,
-                                                                                  kernel,
-                                                                                  num_correct / len(test_labels))
-            push_note(subject, body)
+    print('classification report')
+    report = classification_report(y_true=test_labels, y_pred=classifier.predict(test_data))
+    print(report)
+
+    if args.email:
+        body = report
+        email(to_addrs=args.email, subject='prometheus done training', body=report)
+
+    if args.push:
+        push_note(subject='prometheus done training', body=report)
+
+    # save the classifier.
+    with open('classifier-{}.dat'.format(classifier.best_params_), 'wb') as f:
+        pickle.dump(classifier, f, protocol=3)
