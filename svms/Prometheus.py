a60148fe51c512b1b4e8d62770bfcb496358637f
Oren Shklarsky <oren@neur.io>
2015-12-01 11:08:11 -0800

    move load_data into utils

--- svms/Prometheus.py
+++ svms/Prometheus.py
@@ -1,5 +1,4 @@
 import numpy as np
-import os
 import sys
 import argparse
 import pickle
@@ -10,21 +9,8 @@ from sklearn.grid_search import GridSearchCV
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import classification_report
 from utils.notification import email, push_note
+from utils import load_data
 
-
-def merge(arrays, idx_to_omit):
-    """
-    Merge the np.arrays in a list into a single 2D np.array, omitting the row at idx.
-    (len(arrays) - 1) * arrays[0].shape[0] by arrays[0].shape[1]
-
-    :type arrays: list
-    :param arrays: list of np.arrays to merge
-    :param idx_to_omit: index of row to omit.
-    :return: merged np.array.
-    """
-
-    result = np.concatenate(arrays[0:idx_to_omit] + arrays[idx_to_omit+1:])
-    return result
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='Prometheus svm')
@@ -38,40 +24,7 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    # location of data
-    DATA_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'data')
-
-    print("Loading data", end='')
-    # load the data into a numpy array.
-    with open(os.path.join(DATA_DIR, args.data_file), 'r') as training_file:
-        data_rows = training_file.readlines()
-
-    # number of examples (250000)
-    N = len(data_rows) - 1  # first row of training.csv is column names
-    # number of features (30 + 1 weight)
-    D = len(data_rows[0].strip().split(',')) - 2  # ignore the id and the label. Keep the weight for now.
-
-    # data is an N x D array
-    all_data = np.zeros((N, D))
-
-    # label vector.
-    all_labels = np.zeros((N, 1), dtype=str)
-
-    # process the rows, and fill up the data and labels. Ignore the first row.
-    for i, row in enumerate(data_rows[1:]):
-        values = row.strip().split(',')
-
-        all_data[i] = values[1:-1]
-
-        all_labels[i] = values[-1]
-
-        # show some progress
-        if i > 0 and i % 10000 == 0:
-            print('.', end='')
-            sys.stdout.flush()
-
-    print('done')
-    sys.stdout.flush()
+    all_data, all_labels = load_data(args.data_file)
 
     # we're going to use grid search to find best parameters.
     train_data, test_data, train_labels, test_labels = train_test_split(all_data, all_labels, test_size=args.test_size)
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,47 @@
 import pickle
+import os
+import sys
+import numpy as np
+
+DATA_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'data')
 
 
 def load_classifier(file_path):
     with open(file_path, 'rb') as f:
         return pickle.load(f)
+
+
+def load_data(data_file):
+
+    print("Loading data", end='')
+    # load the data into a numpy array.
+    with open(os.path.join(DATA_DIR, data_file), 'r') as training_file:
+        data_rows = training_file.readlines()
+
+    # number of examples (250000)
+    N = len(data_rows) - 1  # first row of training.csv is column names
+    # number of features (30 + 1 weight)
+    D = len(data_rows[0].strip().split(',')) - 2  # ignore the id and the label. Keep the weight for now.
+
+    # data is an N x D array
+    data = np.zeros((N, D))
+
+    # label vector.
+    labels = np.zeros((N, 1), dtype=str)
+
+    # process the rows, and fill up the data and labels. Ignore the first row.
+    for i, row in enumerate(data_rows[1:]):
+        values = row.strip().split(',')
+
+        data[i] = values[1:-1]
+
+        labels[i] = values[-1]
+
+        # show some progress
+        if i > 0 and i % 10000 == 0:
+            print('.', end='')
+            sys.stdout.flush()
+
+    print('done')
+    sys.stdout.flush()
+    return data, labels
