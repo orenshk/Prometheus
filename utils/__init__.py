2646d35fa52a3bf8c152409f3f017e4e6cde7600
Oren Shklarsky <oren@neur.io>
2015-12-02 12:38:10 -0800

    Logistic regression classifier for HiggsML problem.

--- utils/__init__.py
+++ utils/__init__.py
@@ -13,10 +13,14 @@ def load_classifier(file_path):
         return pickle.load(f)
 
 
-def load_data(data_file, test_size, one_of_k=False):
+def load_data(data_file, test_size, encoding='string'):
     """
     Load the data from data_file and split it into training set and test set. The test size is a fraction of the set.
-    :param one_of_k: return labels as 1-of-k vectors
+    :type encoding: str
+    :param encoding: type of encoding to use for classes.
+        string will keep 's' and 'b' labels.
+        one-hot will make two vectors, (1,0) for s, (0, 1) for b
+        integer will assign integer classes - s: 0, b: 1
     :param data_file: name of data file. Assumed to be in DATA_DIR
     :param test_size: float, proprtion of data to be used for test.
     :return: tuple containing:
@@ -33,32 +37,21 @@ def load_data(data_file, test_size, one_of_k=False):
         data_rows = training_file.readlines()
 
     # number of examples (250000)
-    N = len(data_rows) - 1  # first row of training.csv is column names
+    n = len(data_rows) - 1  # first row of training.csv is column names
     # number of features (30 + 1 weight)
-    D = len(data_rows[0].strip().split(',')) - 2  # ignore the id and the label. Keep the weight for now.
+    d = len(data_rows[0].strip().split(',')) - 2  # ignore the id and the label. Keep the weight for now.
 
     # data is an N x D array
-    data = np.zeros((N, D))
+    data = np.zeros((n, d))
 
-    # label vector.
-    if one_of_k:
-        labels = np.zeros((N, 2), dtype=float)
-    else:
-        labels = np.zeros((N, 1), dtype=str)
+    labels, label_map = _init_label_vector(encoding, n)
 
     # process the rows, and fill up the data and labels. Ignore the first row.
     for i, row in enumerate(data_rows[1:]):
         values = row.strip().split(',')
 
         data[i] = values[1:-1]
-
-        if one_of_k:
-            if values[-1] == 's':
-                labels[i] = np.array([1, 0])
-            else:
-                labels[i] = np.array([0, 1])
-        else:
-            labels[i] = values[-1]
+        labels[i] = label_map[values[-1]]
 
         # show some progress
         if i > 0 and i % 10000 == 0:
@@ -79,3 +72,27 @@ def load_data(data_file, test_size, one_of_k=False):
     test_data = test_data[:, 0:-1]
 
     return train_data, test_data, train_weights, train_labels, test_labels
+
+
+def _init_label_vector(encoding, n):
+    """
+    initialize the label vector.
+
+    :param encoding:  type of encoding to use. Either string, integer, or one-hot
+    :param n: number of samples in set.
+    :return: an np.array containing all zeros, of the required type, and a dictionary mapping labels to encoding.
+    """
+    # label vector.
+    if encoding == 'one-hot':
+        labels = np.zeros((n, 2), dtype=float)
+        label_map = {'s': [1, 0], 'b': [0, 1]}
+    elif encoding == 'integer':
+        labels = np.zeros((n,), dtype=int)
+        label_map = {'s': 0, 'b': 1}
+    elif encoding == 'string':
+        labels = np.zeros((n, 1), dtype=str)
+        label_map = {'s': 's', 'b': 'b'}
+    else:
+        raise TypeError('unsupported encoding: {}'.format(encoding))
+
+    return labels, label_map
